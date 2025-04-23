62732fdbd133faf9dfbefbb7fddc942e66f00fe8
Douglas Barbosa <136455750+Dum2601@users.noreply.github.com>
2025-04-22 21:28:02 -0300

    Minor fixes in Nav bar

--- assets/code/modaContent.js
+++ assets/code/modaContent.js
@@ -1,29 +1,29 @@
-const modalContent = document.querySelector('.content')
+// const modalContent = document.querySelector('.content')
 
-// ---------------------------------------------------------------------------
-// Page Modal
+// // ---------------------------------------------------------------------------
+// // Page Modal
 
-const pageBtn = document.getElementById('pageBtn')
+// const pageBtn = document.getElementById('pageBtn')
 
-pageBtn.addEventListener('click', () => {
+// pageBtn.addEventListener('click', () => {
 
-    modalContent.innerHTML = `<p>Teste Page</p>`
+//     modalContent.innerHTML = `<p>Teste Page</p>`
 
-})
-
-
-// ----------------------------------------------------------------------------
-
-// ----------------------------------------------------------------------------
-// Search Modal
-
-const searchBtn = document.getElementById('searchBtn')
-
-searchBtn.addEventListener('click', () => {
-
-    modalContent.innerHTML = `<p>Teste Search</p>`
-
-})
+// })
 
 
-// ------------------------------------------------------------------------------
+// // ----------------------------------------------------------------------------
+
+// // ----------------------------------------------------------------------------
+// // Search Modal
+
+// const searchBtn = document.getElementById('searchBtn')
+
+// searchBtn.addEventListener('click', () => {
+
+//     modalContent.innerHTML = `<p>Teste Search</p>`
+
+// })
+
+
+// // ------------------------------------------------------------------------------
