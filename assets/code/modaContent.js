8694c599f1a009e07869727c5ff1bc797deb62ff
Douglas Barbosa <136455750+Dum2601@users.noreply.github.com>
2025-04-22 21:29:38 -0300

    Modal logics added.

--- assets/code/modaContent.js
+++ assets/code/modaContent.js
@@ -1,29 +1,50 @@
-// const modalContent = document.querySelector('.content')
+const modalContent = document.querySelector('.content')
 
-// // ---------------------------------------------------------------------------
-// // Page Modal
+// ---------------------------------------------------------------------------
+// Page Modal
 
-// const pageBtn = document.getElementById('pageBtn')
+const pageBtn = document.getElementById('pageBtn')
 
-// pageBtn.addEventListener('click', () => {
+pageBtn.addEventListener('click', () => {
 
-//     modalContent.innerHTML = `<p>Teste Page</p>`
+    modalContent.innerHTML = `<p>Teste Page</p>`
 
-// })
-
-
-// // ----------------------------------------------------------------------------
-
-// // ----------------------------------------------------------------------------
-// // Search Modal
-
-// const searchBtn = document.getElementById('searchBtn')
-
-// searchBtn.addEventListener('click', () => {
-
-//     modalContent.innerHTML = `<p>Teste Search</p>`
-
-// })
+})
 
 
-// // ------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------
+// Search Modal
+
+const searchBtn = document.getElementById('searchBtn')
+
+searchBtn.addEventListener('click', () => {
+
+    modalContent.innerHTML = `<p>Teste Search</p>`
+
+})
+
+
+// ------------------------------------------------------------------------------
+// Ul modal
+
+const pokeData = document.querySelectorAll('.pokeData')
+
+pokeData.forEach(poke => {
+
+    const ulElements = poke.querySelectorAll('ul.modalBtn')
+    
+    ulElements.forEach((ul) => {
+
+        ul.addEventListener('click', () => {
+
+            const secondClass = ul.classList[1]
+            console.log(secondClass)
+            modalContent.innerHTML = `<p>Teste ${secondClass}</p>`
+
+        })
+    })
+})
+
+
